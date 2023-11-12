757af399f52a38bc655e55dc803a4e8d914276a0
Hadassah Uche <uhadassah1@gmail.com>
2023-11-12 10:13:51 +0200

    Added relevant comments and cleaned up the code. Im glad that i finally calmed down and wrote the code

--- UcheHadassah3_7/HadassahUche3_7.cpp
+++ UcheHadassah3_7/HadassahUche3_7.cpp
@@ -1,48 +1,28 @@
-/*Sieve of Eratosthenes: Write a program that finds and prints all the prime numbers 
-between 1 and 500. A prime integer is an integer greater than 1 that is divisible only 
-by 1 and itself (e.g. 2, 3, 5, 7, 11, 13 etc). The Sieve of Eratosthenes is a method of 
-finding prime numbers. It operates as follows: 
-a) Create an array with all elements initialized to 1 (true). Array elements with prime 
-subscripts will remain 1 and all other (non-prime) elements will be eventually set 
-to 0 (false).
-b) Starting with array subscript 2, every time an array element is found whose value 
-is 1, loop through the remainder of the array and set to zero every element whose 
-subscript is a multiple of the subscript for the element with value 1. For example, 
-for array subscript 2, all elements beyond 2 in the array that are multiples of 2 will 
-be set to 0 (subscripts 4, 6, 8, 10, etc.); for array subscript 3, all elements beyond 3 
-in the array that are multiples of 3 will be set to zero (subscripts 6, 9, 12, 15, etc); 
-and so on.
-When this process is complete, the array elements that are still set to 1 indicate that the
-subscript is a prime number. These subscripts can then be printed out.
-Output (all prime numbers between 1 and 500):
-2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101 103 
-107 109 113 127 131 137 139 149 151 157 163 167 173 179 181 191 193 197 199 
-211 223 227 229 233 239 241 251 257 263 269 271 277 281 283 293 307 311 313 
-317 331 337 347 349 353 359 367 373 379 383 389 397 401 409 419 421 431 433 
-439 443 449 457 461 463 467 479 487 491 499*/
+/*Name: Uche Hadassah
+This program finds all prime numbers from 1 - 500 using the Sieve of Eratosthenes and ouputs them*/
 #include<iostream>
 using namespace std;
 int main()
 {
 	const int MAX = 500;
-	bool array[MAX];
-	for (int i = 0; i < MAX; i++)
+	bool array[MAX];//Made the array a bool
+	for (int i = 0; i < MAX; i++)//Initializing all the numbers in the array to 1
 	{
 		array[i] = true;
 	}
 	for (int j = 2; j < MAX; j++)//Starts checking from index 2
 	{
-		int val = j;
-		while ((val+j) % j == 0 && val+j < MAX)
+		int tmp = j;//created a temporary value for easy index manipulation later
+		while ((tmp+j) % j == 0 && tmp+j < MAX)//While the number is divisible by j
 		{
-			array[val+j] = false;//Sets the value of the array to 0
-			val += j;
+			array[tmp+j] = false;//Sets the value of the array to 0
+			tmp += j;//Increases the temp index by j
 		}
 	}
 	cout << "The prime numbers are:" << endl;
-	for (int k = 2; k < MAX; k++)
+	for (int k = 2; k < MAX; k++)//Start with 2 since 0,1 and 500 are not included
 	{
-		if(array[k] == true)
+		if(array[k] == true)//If the value in the array is still 1
 		{
 			cout << k << " ";
 		}
